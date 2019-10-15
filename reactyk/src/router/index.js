0aa73932135d7a1dea837c2a6ee269ae4e89ff92
zhou <3192713828@qq.com>
2019-10-15 11:11:44 +0800

    'src'

---
+++ reactyk/src/router/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {BrowserRouter,Route,Switch,Redirect} from 'react-router-dom'
+
+
+function RootRouter(){
+    
+}
---
+++ reactyk/src/views/index/frunt/frunt.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Frunt extends Component {
+    render() {
+        return (
+            <div>
+                funrt
+            </div>
+        )
+    }
+}
